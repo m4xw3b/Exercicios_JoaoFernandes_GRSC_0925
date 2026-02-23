b61c66caaf229b6fbe948718d39ac29b12d4c53a
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-02-23 14:31:04 +0000

    Adicionado Exercicio 13: Comra/Venda/Valor

---
+++ Exercicio13.ps1
@@ -0,0 +1,20 @@
+﻿#Tipo de pedido Recebe um dicionário com as chaves "tipo" e "valor". Exibe: 
+#• “Compra de X€” se tipo for “compra” 
+#• “Venda de X€” se tipo for “venda” 
+#• “Pedido desconhecido” caso contrário Exemplo: Entrada → {"tipo": "venda", "valor": 250} Saída → Venda de 250€
+$tipoInput = Read-Host "Introduza o tipo de pedido (compra ou venda)"
+$valorInput = Read-Host "Introduza o valor"
+$pedido = @{
+    tipo  = $tipoInput
+    valor = $valorInput
+}
+
+if ($pedido["tipo"] -eq "compra") {
+    Write-Host "Compra de $($pedido['valor'])€" -ForegroundColor Cyan
+}
+elseif ($pedido["tipo"] -eq "venda") {
+    Write-Host "Venda de $($pedido['valor'])€" -ForegroundColor Green
+}
+else {
+    Write-Host "Pedido desconhecido" -ForegroundColor Red
+}
