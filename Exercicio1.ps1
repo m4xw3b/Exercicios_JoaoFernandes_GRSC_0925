d06522eadc68e8e2191799e89f4e1418d27d0dad
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-02-23 09:50:55 +0000

    Adicionado Exercicio 3: Crescente e Decrescente

---
+++ Exercicio1.ps1
@@ -0,0 +1,7 @@
+﻿#Exercicio 1 - Pedir Segundos e Responder em Horas
+[int]$lersegundos = Read-Host "Coloque o numero em segudos"
+$horas = [Math]::Floor($lersegundos/3600)
+$segundosrestantes = $lersegundos % 3600
+$minutos = [Math]::Floor($segundosrestantes / 60)
+$segundosquefaltam = $segundosrestantes % 60
+Write-Host "Os segundos introduzidos $lersegundos, correspondem a $horas hora(s), $minutos minuto(s) e $segundosquefaltam segundo(s)"
---
+++ Exercicio3.ps1
@@ -0,0 +1,12 @@
+﻿#Exercício 3: Mostrar Números em Ordem Crescente e Decrescente
+[int]$num1 = Read-Host "Introduza o primeiro numero"
+[int]$num2 = Read-Host "Introduza o segundo numero"
+if ($num1 -le $num2) {
+    $crescente = "$num1, $num2"
+    $decrescente = "$num2, $num1"
+} else {
+    $crescente = "$num2, $num1"
+    $decrescente = "$num1, $num2"
+}
+Write-Host "Crescente: $crescente"
+Write-Host "Decrescente: $decrescente"
