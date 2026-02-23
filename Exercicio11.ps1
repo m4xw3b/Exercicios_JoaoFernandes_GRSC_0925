96b45337948735c594006087c027fb6d1f485db4
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-02-23 13:09:58 +0000

    Adicionado Exercicio 11: Fim de semana ou dia util ?

---
+++ Exercicio11.ps1
@@ -0,0 +1,22 @@
+﻿#Tipo de dia Cria um programa que receba o nome de um dia da semana e diga se é dia útil ou fim de semana. 
+#Exemplo: Entrada → domingo Saída → Fim de semana
+[string]$dia = Read-Host "Introduza o dia da semana (exemplo: segunda)"
+Switch ($dia) {
+    "sabado"  { $diadasemana = "Fim de Semana" }
+    "domingo" { $diadasemana = "Fim de Semana" }
+    "segunda" { $diadasemana = "Dia Util" }
+    "terça"   { $diadasemana = "Dia Util" }
+    "quarta"  { $diadasemana = "Dia Util" }
+    "quinta"  { $diadasemana = "Dia Util" }
+    "sexta"   { $diadasemana = "Dia Util" }
+Default { $diadasemana = "Erro: Dia da semana inválido. Repita, pff." } 
+}
+if ($diadasemana -eq "Fim de Semana") {
+    Write-Host "Resultado: Fim de Semana" -ForegroundColor Green
+}
+elseif ($diadasemana -eq "Dia Util") {
+    Write-Host "Resultado: Dia Util" -ForegroundColor Red
+}
+else {
+    Write-Host "Erro: Dia da semana inválido. Repita, pff." -ForegroundColor Yellow
+}
