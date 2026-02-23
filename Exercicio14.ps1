f27c0dcb2a496716990e71abaa39e7994cc5319f
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-02-23 14:41:55 +0000

    Adicionado Exercicio 14 - Tipo de Introdução

---
+++ Exercicio14.ps1
@@ -0,0 +1,34 @@
+﻿#Tipo de dado Analisa um valor e retorna o seu tipo: 
+#• Número inteiro 
+#• Número decimal 
+#• String numérica 
+#• String textual 
+#• Lista • 
+#Tipo desconhecido Exemplo: 
+#Entrada → [10, 20, 30] Saída → Lista
+$inputOriginal = Read-Host "Introduza um valor (ex: 100, 100.5, 1,2,3 ou texto)"
+if ($inputOriginal -match ',') {
+    $valor = $inputOriginal.Split(',') 
+}
+elseif ($inputOriginal.Contains('.') -and ($inputOriginal -as [float])) {
+    $valor = [float]$inputOriginal 
+}
+elseif ($inputOriginal -as [int]) {
+    $valor = [int]$inputOriginal 
+}
+else {
+    $valor = $inputOriginal 
+}
+if ($valor -is [float] -or $valor -is [double]) {
+    $resultado = "Número Decimal"
+}
+elseif ($valor -is [int]) {
+    $resultado = "Número Inteiro"
+}
+elseif ($valor -is [array]) {
+    $resultado = "Lista"
+}
+else {
+    $resultado = "String textual"
+}
+Write-Host "O tipo de conteudo introduzido é: $resultado"
