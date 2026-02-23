f05c13e7b75fb89436174023f1c041e393b8e1f6
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-02-23 10:31:23 +0000

    Adicionado Exercicio 4: Cheque

---
+++ Exercicio4.ps1
@@ -0,0 +1,9 @@
+﻿#Exercício 4: Verificar se o Cheque Pode Ser Descontado
+[int]$saldo = Read-Host "Introduza o valor do saldo"
+[int]$cheque = Read-Host "Introduza o valor do cheque"
+if ($saldo -ge $cheque) {
+    $descontar = $saldo - $cheque
+    Write-Host "Depois do descontar o valor do cheque, o novo saldo é $descontar"
+} else {
+    write-host "Não existe saldo suficiente"
+    }
