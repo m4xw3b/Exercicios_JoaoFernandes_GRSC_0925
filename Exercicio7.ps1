9325974195008bb16a4a5e045ffd42c3680293e6
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-02-23 12:25:34 +0000

    Adicionado Exercicio 7: Media de Notas

---
+++ Exercicio7.ps1
@@ -0,0 +1,19 @@
+﻿#Exercício 7: Calcular a Média de Notas com Pesos
+#Enunciado: O sistema de avaliação de uma disciplina tem três provas com pesos diferentes.
+#A primeira tem peso 2, a segunda tem peso 3, e a terceira tem peso 5.
+#Crie um programa para calcular a média final de um aluno e mostrar se ele está APROVADO (nota >= 6) ou 
+#REPROVADO (nota < 6).
+[float]$nota1 = Read-Host "Qual o valor da primeira nota de avaliação (de 0 a 10) - peso 2"
+[float]$nota2 = Read-Host "Qual o valor da primeira nota de avaliação (de 0 a 10) - peso 3"
+[float]$nota3 = Read-Host "Qual o valor da primeira nota de avaliação (de 0 a 10) - peso 5"
+$aval1=0
+$aval2=0
+$aval3=0
+[int]$aval1 = $nota1*2
+[int]$aval2 = $nota2*3
+[int]$aval3 = $nota3*5
+if ((($aval1+$aval2+$aval3)/10) -ge 6 ) {
+    Write-Host "ALUNO APROVADO" -ForegroundColor Green
+}else{
+    Write-Host "ALUNO REPROVADO" -ForegroundColor Red
+}
