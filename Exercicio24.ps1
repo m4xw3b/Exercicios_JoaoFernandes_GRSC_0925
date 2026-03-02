c5d42635bd0dae236e2d585adc3ed987b60993e5
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-03-02 15:16:12 +0000

    25. Exercicio - Listar 10000 numeros inteiros

---
+++ Exercicio24.ps1
@@ -0,0 +1,2 @@
+﻿#Crie um algoritmo que leia um número inteiro, e diga se ele é um número primo ou não.
+[int]$numero = Read-Host "Introduza um número"
---
+++ Exercicio25.ps1
@@ -0,0 +1,4 @@
+﻿#Elabore um programa que escreve os primeiros 10.000 números inteiros no ecrã.
+for ($i = 1; $i -le 10000; $i++) {
+    $i
+    }
