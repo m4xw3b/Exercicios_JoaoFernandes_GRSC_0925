3074538f07b130dc6ab04eda6a609998dbbfdce2
Joao Fernandes <joao.fernandes.t0132022@edu.atec.pt>
2026-03-09 14:28:26 +0000

    Exercicio 24. Numeros Primos

--- Exercicio24.ps1
+++ Exercicio24.ps1
@@ -1,2 +1,15 @@
 ﻿#Crie um algoritmo que leia um número inteiro, e diga se ele é um número primo ou não.
 [int]$numero = Read-Host "Introduza um número"
+$divisores = 0
+for ($i = 1; $i -le $numero; $i++) {
+    if ($numero % $i -eq 0) {
+        $divisores++ 
+    }
+}
+
+if ($divisores -eq 2) {
+    Write-Host "O número $numero é PRIMO." -ForegroundColor Green
+}
+else {
+    Write-Host "O número $numero NÃO é primo." -ForegroundColor Red
+}
